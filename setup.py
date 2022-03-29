5b5960cf232f407f98de4852a786dea5f60a912d
Daniel Shen <dshen109@gmail.com>
2022-03-29 16:01:58 -0400

    add setup.py for easier packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rc_simulator',
+    version='0.0.0',
+    description='HVAC control using deep RL',
+    author='Prageeth Jayathissa',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[],
+)
