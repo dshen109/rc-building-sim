79f2bfadd7e27677b2c4f129a3059f470cf13b0c
Daniel Shen <dshen109@gmail.com>
2022-04-30 23:56:44 -0400

    python3.7

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='HVAC control using deep RL',
     author='Prageeth Jayathissa',
     packages=find_packages(),
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     install_requires=[],
 )
